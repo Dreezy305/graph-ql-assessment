429462aa0afefbeea5567a451081f73e85c277e4
Dreezy <bankoleidris@gmail.com>
2022-07-10 22:10:08 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from "react";
+import "./App.css";
+import { ReactComponent as Glass } from "./assets/glass.svg";
+import { ReactComponent as Funnel } from "./assets/funnel.svg";
+import UserImg from "./assets/userImg.png";
 
 function App() {
+  const [filter, setFilter] = React.useState("");
+  const searchBar = () => {
+    return (
+      <div className="relative w-3/4 pt-8">
+        <input
+          type={"text"}
+          id="search-bar"
+          name="search-bar"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+          className="rounded-md w-full mx-auto border-solid bg-[#F2F2F2] py-3 px-3 outline-none shadow-none"
+        />
+        <Glass className="absolute top-12 right-4" />
+      </div>
+    );
+  };
+
+  const filterSelectors = () => {
+    return (
+      <div className="flex flex-row space-x-7 items-center mt-8">
+        <select className="w-24 h-9 rounded-md border-solid border-black outline-none px-1">
+          <option value="" disabled selected hidden>
+            Types
+          </option>
+          <option value={"male"}>Male</option>
+          <option value={"female"}>Female</option>
+        </select>
+        <select className="w-24 h-9 rounded-md border-solid border-black outline-none px-1">
+          <option value="" disabled selected hidden>
+            Status
+          </option>
+          <option value={"active"}>Urgent</option>
+          <option value={"inactive"}>High</option>
+          <option value="low">Low</option>
+          <option value={"medium"}>Medium</option>
+        </select>
+        <select className="w-24 h-9 rounded-md border-solid border-black outline-none px-1">
+          <option value="" disabled selected hidden>
+            Dates
+          </option>
+        </select>
+      </div>
+    );
+  };
+
+  const dataCard = () => {
+    return (
+      <>
+        <div className="w-36 h-11 lg:flex lg:flex-row lg:items-center lg:justify-between rounded-md bg-[#0F1642] px-4 mt-8 cursor-pointer py-4 text-center">
+          <h6 className="font-medium text-white text-sm">Tue,2nd-2022 </h6>
+        </div>
+        <div className="bg-white w-1/3 rounded-xl shadow-lg shadow-indigo-500/40 py-7 mt-4 border-solid border-black">
+          <div className="flex flex-row px-8 space-x-8">
+            <img
+              src={UserImg}
+              className="h-20 w-20 rounded-2xl"
+              alt="user image"
+            />
+            <div className="flex flex-col space-y-0">
+              <div className="flex flex-row space-x-1">
+                <span>Name:</span>
+                <span>Jane Doe</span>
+              </div>
+              <div className="flex flex-row space-x-1">
+                <span>Status:</span>
+                <span>Closed</span>
+              </div>
+              <div className="flex flex-row space-x-1">
+                <span className="italic">Priority:</span>
+                <span>Low</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto lg:mx-auto">
+      {searchBar()}
+      <div className="w-24 h-11 lg:flex lg:flex-row lg:items-center lg:justify-between rounded-md bg-[#0F1642] px-4 mt-8 cursor-pointer py-4">
+        <Funnel fill="#fff" className="cursor-pointer" />
+        <h5 className="font-mono text-white text-xs font-medium leading-3 cursor-pointer">
+          Filter
+        </h5>
+      </div>
+      {filterSelectors()}
+      {dataCard()}
     </div>
   );
 }
